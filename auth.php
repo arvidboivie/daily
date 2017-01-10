05ee2453cec74c9d56c5a7be8a3160559f087719
Arvid Boivie <arvid.boivie@gmail.com>
2017-01-10 21:07:30 +0100

    Try to get playlists

--- auth.php
+++ auth.php
@@ -3,11 +3,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$baseUrl = 'https://api.spotify.com/v1/';
+
 // TODO: Get authenticated
 $authUrl = 'https://accounts.spotify.com/api/token';
 
 $clientId = '***REMOVED***';
 $clientSecret = '***REMOVED***';
+$accessToken = null;
 
 $ch = curl_init();
 
@@ -22,12 +25,28 @@ $curlConfig = [
     CURLINFO_HEADER_OUT => true,
 ];
 
-echo base64_encode($clientId.':'.$clientSecret);
+curl_setopt_array($ch, $curlConfig);
+
+$result = curl_exec($ch);
+curl_close($ch);
+
+$result = json_decode($result);
+
+$accessToken = $result['access_token'];
+
+$ch = curl_init();
+
+$curlConfig = [
+    CURLOPT_URL => $authUrl . 'users/arvid.b/playlists',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => [
+        'Authorization: Basic ' . $accessToken,
+    ],
+];
 
 curl_setopt_array($ch, $curlConfig);
 
 $result = curl_exec($ch);
+curl_close($ch);
 
 var_dump($result);
-
-curl_close($ch);
