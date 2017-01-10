58f0270febae6ce475b594fceb8c927c782295d1
Arvid Boivie <arvid.boivie@gmail.com>
2017-01-10 20:15:22 +0100

    Test Spotify Auth

---
+++ auth.php
@@ -0,0 +1,25 @@
+<?php
+
+// TODO: Get authenticated
+$authUrl = 'https://accounts.spotify.com/api/token';
+
+$clientId = '***REMOVED***';
+$clientSecret = '***REMOVED***';
+
+$ch = curl_init();
+
+$curlConfig = [
+    CURLOP_URL => $curlUrl,
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => [ 'Authorization: Basic ' . base64_encode($clientId.':'.$clientSecret) ],
+    CURLOPT_POSTFIELDS => [ 'grant_type' => 'client_credentials' ],
+];
+
+curl_setopt_array($ch, $curlConfig);
+
+$result = curl_exec($ch);
+
+curl_close($ch);
+
+echo $result;
