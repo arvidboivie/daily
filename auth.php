7b4b5de182bcc103aeb801f4551246a5c364a6be
Arvid Boivie <arvid.boivie@gmail.com>
2017-01-17 20:37:50 +0100

    Test time calculation

--- auth.php
+++ auth.php
@@ -52,7 +52,7 @@ $pdo = new \PDO($dsn, $user, $password);
 $tokenStatement = $pdo->prepare('INSERT INTO auth(username, access_token, refresh_token, expires)
                                   VALUES(:username, :access_token, :refresh_token, :expires)');
 
-var_dump($expiration);
+var_dump(time()+$expiration);
 die();
 
 $tokenStatement->execute([
