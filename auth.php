43da0b95c87454b25aa5a730dca86b00295508f6
Arvid Boivie <arvid.boivie@gmail.com>
2017-01-17 20:43:45 +0100

    Add ON DUPLICATE KEY UPDATE

--- auth.php
+++ auth.php
@@ -50,7 +50,11 @@ $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $pdo = new \PDO($dsn, $user, $password);
 
 $tokenStatement = $pdo->prepare('INSERT INTO auth(username, access_token, refresh_token, expires)
-                                  VALUES(:username, :access_token, :refresh_token, :expires)');
+                                 VALUES(:username, :access_token, :refresh_token, :expires)
+                                 ON DUPLICATE KEY UPDATE
+                                 access_token= :username,
+                                 refresh_token= :refresh_token,
+                                 expires= :expires');
 
 $tokenStatement->execute([
     'username' => $userInfo->id,
