78352f18a855d9cda91b9df580d5f455a04e619b
Arvid Boivie <arvid.boivie@gmail.com>
2017-02-02 21:51:07 +0100

    Add slash to uri

--- public/update.php
+++ public/update.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require dirname(__FILE__).'../vendor/autoload.php';
+require dirname(__FILE__).'/../vendor/autoload.php';
 
 use Boivie\Spotify\Api;
 use Boivie\DailyDouble\Update;
