f10b313ce49d93d176b951711306e09d1170e2ae
Arvid Boivie <arvid.boivie@gmail.com>
2017-09-16 10:18:47 +0200

    Remove unused function

--- src/Controller/Search.php
+++ src/Controller/Search.php
@@ -14,37 +14,6 @@ class Search
         $this->db = $db;
     }
 
-    //TODO: Remove or use this function
-    public function search($searchTerm)
-    {
-        $songStatement = $this->db->prepare(
-            'SELECT
-            tracks.id AS track_id,
-            tracks.name AS track_name,
-            album,
-            added_by,
-            playlists.name AS playlist_name
-            FROM tracks
-            LEFT JOIN playlists ON playlists.id = tracks.playlist_id
-            ORDER BY playlist_name'
-        );
-
-        $songStatement->execute();
-
-        $songs = $songStatement->fetchAll();
-
-        $results = array_filter($songs, function ($songObject) use ($searchTerm) {
-            if (preg_match('/'.$searchTerm.'/i', $songObject['track_name']) === 1) {
-                return true;
-            }
-            if (preg_match('/'.$searchTerm.'/i', $songObject['album']) === 1) {
-                return true;
-            }
-        });
-
-        return $results;
-    }
-
     public function getSongs($searchTerm)
     {
         $songStatement = $this->db->prepare(
