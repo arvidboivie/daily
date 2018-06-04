4c41523546cf31dfcb50066f7712a8ea2011968d
Arvid Boivie <arvid.boivie@gmail.com>
2018-06-04 19:02:10 +0200

    Fix sql syntax in search query

--- src/Action/SearchAction.php
+++ src/Action/SearchAction.php
@@ -20,7 +20,7 @@ class SearchAction
             'SELECT
             tracks.name AS label,
             tracks.playlist_id AS playlist,
-            playlists.creator AS creator
+            playlists.creator AS creator,
             playlists.name AS playlist_name
             FROM tracks
             LEFT JOIN playlists ON playlists.id = tracks.playlist_id
